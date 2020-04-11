2ab3e3abbb1c8f8b3d97f274454c938eb246429e
yashthecoder <61641470+yashthecoder@users.noreply.github.com>
2020-04-11 09:58:37 +0530

    Add files via upload

---
+++ js/Player.js
@@ -0,0 +1,38 @@
+class Player {
+  constructor(){
+    this.index=null;
+    this.distance = 0;
+    this.name = null;
+
+  }
+
+  getCount(){
+    var playerCountRef = database.ref('playerCount');
+    playerCountRef.on("value",function(data){
+      playerCount = data.val();
+    })
+  }
+
+  updateCount(count){
+    database.ref('/').update({
+      playerCount: count
+    });
+  }
+
+  update(){
+    var playerIndex = "player/players" + this.index;
+    database.ref(playerIndex).set({
+      name:this.name,
+      distance: this.distance
+    });
+  }
+
+  static getPlayerInfo(){
+    var PlayerInfoRef = database.ref('players');
+    PlayerInfoRef.on("value",function(data){
+      allPlayers= data.val();
+    })
+  }
+
+
+}
